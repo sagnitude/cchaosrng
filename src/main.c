4b903dffb5cbf0b5246103c54c8ab96e6ea25d73
sagnitude <leyicong@gmail.com>
2014-08-12 18:17:22 +0800

    Profiling added.

--- src/main.c
+++ src/main.c
@@ -2,12 +2,15 @@
 #include <stdlib.h>
 #include "cm.h"
 #include "cp.h"
+#include <time.h>
 
 #define MAINSERVER
+// #define PRINTOUT
+#define PROFILE
 
 int
 main(int argc, char** argv){
-  ChaosPool* pool = ChaosPoolBySize(2, 3);
+  ChaosPool* pool = ChaosPoolBySize(40, 3);
   initializeChaosPoolWithRandomValues(pool);
 
   /*
@@ -17,11 +20,26 @@ main(int argc, char** argv){
   initializeChaosPoolWithRandomValues(pool);
   */
 
+  #ifdef PRINTOUT
   printChaosPool(pool);
-  evolve(1000, pool);
+  #endif
 
+  #ifdef PROFILE
+  time_t t_start, t_end;
+  t_start = time(NULL);
+  #endif
+
+  evolve(100000, pool);
+
+  #ifdef PROFILE
+  t_end = time(NULL) ;
+  printf("calculating %d points\ntime elasped from %ld to %ld: %ld milliseconds\n", pool->overallSize, t_start, t_end, t_end-t_start);
+  #endif
+
+  #ifdef PRINTOUT
   printf("\n\n");
   printChaosPool(pool);
+  #endif
 
   return 0;
 }
