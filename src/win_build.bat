db3215c085364084d6e3c0b5ed65aab202fa8bd3
sagnitude <leyicong@gmail.com>
2014-08-13 13:38:02 +0800

    win built updated.
    now runnable in double click mode..

--- src/win_build.bat
+++ src/win_build.bat
@@ -3,30 +3,24 @@
 @if exist C:\MinGW\bin\mingw32-make.exe set cmake_make_program="C:\MinGW\bin\mingw32-make.exe"
 @if exist D:\MinGW\bin\mingw32-make.exe set cmake_make_program="D:\MinGW\bin\mingw32-make.exe"
 @if exist E:\MinGW\bin\mingw32-make.exe set cmake_make_program="E:\MinGW\bin\mingw32-make.exe"
-@REM @set paths=%PATH%
-@start /B /WAIT make
-@REM echo %paths%
-@REM :STR_R
-@REM  echo "looping"
-@REM @for /f "tokens=1,* delims=;" %%a in ("%paths%") do (
-@REM   set temp="%%a"
-@REM   set paths=%%b
-  @REM  echo "%%a\cmake.exe"
-@REM   if not exist "%%a\cmake.exe" goto :STR_R
-  @REM  goto STR_R
-@REM   set cmakeexe=%%a\cmake.exe
-@REM )
-@REM @echo "%cmakeexe%"
+@set paths=%PATH%
+:STR_R
+@for /f "tokens=1,* delims=;" %%a in ("%paths%") do (
+  set temp="%%a"
+  set paths=%%b
+  if not exist "%%a\cmake.exe" goto :STR_R
+  set cmakeexe=%%a\cmake.exe
+)
 @cd ..\bin
-@if exist CMakeCache.txt del CMakeCache.txt
+@if exist CMakeCache.txt del CMakeCache.txtf >nul 2>nul
 @rmdir CMakeFiles /S /Q
-@if exist cmake_install.cmake del cmake_install.cmake
-@if exist Makefile del Makefile
-@if exist x.exe del x.exe
-@if exist ..\src\x.exe del ..\src\x.exe
-@echo on
-@start /B /WAIT cmake %cd%\..\src -DCMAKE_MAKE_PROGRAM:FILEPATH=%cmake_make_program% -G "MinGW Makefiles"
+@if exist cmake_install.cmake del cmake_install.cmake >nul 2>nul
+@if exist Makefile del Makefile >nul 2>nul
+@if exist x.exe del x.exe >nul 2>nul
+@if exist ..\src\x.exe del ..\src\x.exe >nul 2>nul
+@start "" /B /WAIT "%cmakeexe%" %cd%\..\src -DCMAKE_MAKE_PROGRAM:FILEPATH=%cmake_make_program% -G "MinGW Makefiles"
 @start /B /WAIT make
 @echo off
 @if exist ..\bin\x.exe mklink ..\src\x.exe ..\bin\x.exe
 @cd ..\src
+@pause
